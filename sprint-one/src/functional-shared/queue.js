6a3930713d3128e86bd814eb05c3059eedd575a1
Justin Kwok <justinkwok@Drop-Test.local>
2017-08-23 12:06:54 -0700

    Functional-shared size added and increases with enqueue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,23 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  let someInstance = {};
+  someInstance.length = 0;
+  extend(someInstance, queueMethods);
+  return someInstance;
 };
 
 var queueMethods = {};
 
+// let extend = function(target, origin) {
+//   for (var key in origin) {
+//     target[key] = origin[key];
+//   }
+// }
 
+queueMethods.size = function() {
+  return this.length;
+}
+queueMethods.enqueue = function() {
+  this.length++;
+}
