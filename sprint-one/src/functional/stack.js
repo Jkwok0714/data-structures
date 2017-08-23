41821432b0d595426223c71ccf579a3f73e4900e
Justin Kwok <justinkwok@Drop-Test.local>
2017-08-23 11:19:13 -0700

    Second test pass: Adding to length

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,18 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    length++;
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
-    return 0;
+    return length;
   };
 
   return someInstance;
