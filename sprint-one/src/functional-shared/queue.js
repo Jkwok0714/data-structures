8f80d4481a415534f09f078dcc4d5ee54e2a17c3
Justin Kwok <justinkwok@Drop-Test.local>
2017-08-23 12:12:03 -0700

    Queue will dequeue and enqueue items

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -3,6 +3,9 @@ var Queue = function() {
   // but try not not reference your old code in writing the new style.
   let someInstance = {};
   someInstance.length = 0;
+  someInstance.numID = 0;
+  someInstance.head = 0;
+  someInstance.storage = {};
   extend(someInstance, queueMethods);
   return someInstance;
 };
@@ -18,9 +21,15 @@ var queueMethods = {};
 queueMethods.size = function() {
   return this.length;
 }
-queueMethods.enqueue = function() {
+queueMethods.enqueue = function(value) {
+  this.storage[this.numID] = value;
+  this.numID++;
   this.length++;
 }
-queueMethods.dequeue = function() {
+queueMethods.dequeue = function(value) {
+  let result = this.storage[this.head];
+  delete this.storage[this.head];
+  this.head++;
   this.length > 0 ? this.length-- : this.length = 0;
+  return result;
 }
