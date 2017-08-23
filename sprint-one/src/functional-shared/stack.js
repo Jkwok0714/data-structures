185beddf156e362477e08d9659593b328eebcc94
Justin Kwok <justinkwok@Drop-Test.local>
2017-08-23 11:40:39 -0700

    Functional-Shared size reports 0 for empty stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,21 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  extend(someInstance, stackMethods);
+
+  return someInstance;
 };
+
+let extend = function(destination, origin) {
+  for (var key in origin) {
+    destination[key] = origin[key];
+  }
+}
 
 var stackMethods = {};
 
-
+stackMethods.size = function() {
+  return 0;
+}
