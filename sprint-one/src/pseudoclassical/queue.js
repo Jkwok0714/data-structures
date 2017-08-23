3e5ed7b5723658948667e7d718ba040ea36c4a8c
Justin Kwok <justinkwok@Drop-Test.local>
2017-08-23 12:35:11 -0700

    Pseudoclassical queue complete, all tests passing

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
+  this.numID = 0;
+  this.header = 0;
+  this.storage = {};
 };
 
-
+Queue.prototype.size = function() {
+  return this.length;
+}
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.numID] = value;
+  this.length++;
+  this.numID++;
+}
+Queue.prototype.dequeue = function() {
+  let result = this.storage[this.header];
+  delete this.storage[this.header];
+  this.header++;
+  this.length > 0 ? this.length-- : this.length = 0;
+  return result;
+}
