829d5fd6dc4924318c27129370005cc1316f5433
Justin Kwok <justinkwok@Drop-Test.local>
2017-08-23 11:34:14 -0700

    Enqueue and Dequeue will now add and remove from the queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,23 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
   var length = 0;
+  var idNum = 0;
+  var header = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[idNum] = value;
+    idNum++;
     length++;
   };
 
   someInstance.dequeue = function() {
     (length > 0) ? length-- : length = 0;
+    let result = storage[header];
+    delete storage[header];
+    header++;
+    return result;
   };
 
   someInstance.size = function() {
