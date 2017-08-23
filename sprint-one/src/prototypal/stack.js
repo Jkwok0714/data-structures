69935c8c785799b064210fc31fd448d287e6622c
Justin Kwok <justinkwok@Drop-Test.local>
2017-08-23 12:19:08 -0700

    Prototypal stack now pops and pushes items

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -2,12 +2,23 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var someInstance = Object.create(stackMethods);
-
+  someInstance.length = 0;
+  someInstance.storage = {};
   return someInstance;
 };
 
 var stackMethods = {};
 
 stackMethods.size = function() {
-  return 0;
+  return this.length;
+}
+stackMethods.push = function(value) {
+  this.storage[this.length] = value;
+  this.length++;
+}
+stackMethods.pop = function(value) {
+  let result = this.storage[this.length-1];
+  delete this.storage[this.length-1];
+  this.length > 0 ? this.length-- : this.length = 0;
+  return result;
 }
