5bf34225d780125922dafa1c93f382f604d1fd61
Justin Kwok <justinkwok@Drop-Test.local>
2017-08-24 12:52:17 -0700

    Passing first two specs in Graphs

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,18 +2,36 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.value = '';
+  this.edges = [];
 };
 
 // Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(node) {
+Graph.prototype.addNode = function(value) {
+  let newNode = new Graph();
+  newNode.value = value;
+  this.edges.push(newNode);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
-Graph.prototype.contains = function(node) {
+Graph.prototype.contains = function(target) {
+  return this.edges.some((ele, index, array) => {
+    return target === ele.value;
+  });
+  // for (let i = 0; i < this.edges.length; i++) {
+  //   if (target === this.edges[i].value) {
+  //     return true;
+  //   }
+  // }
+  // return false;
 };
 
 // Removes a node from the graph.
-Graph.prototype.removeNode = function(node) {
+Graph.prototype.removeNode = function(target) {
+  for (let i = 0; i < this.edges.length; i++) {
+    delete this.edges[i];
+    this.edges.splice(i, 1);
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
@@ -35,5 +53,3 @@ Graph.prototype.forEachNode = function(cb) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
