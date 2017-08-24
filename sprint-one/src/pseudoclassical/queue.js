75e7dfeddab91c136ef5dd0bcdd613b75006158f
Justin Kwok <justinkwok@Drop-Test.local>
2017-08-23 17:47:43 -0700

    Refactored Pseudoclassical-queue to use less variables and check size when dequeueing

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,24 +1,23 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  this.length = 0;
-  this.numID = 0;
+  this.footer = 0;
   this.header = 0;
   this.storage = {};
 };
 
 Queue.prototype.size = function() {
-  return this.length;
+  return this.footer - this.header;
 }
 Queue.prototype.enqueue = function(value) {
-  this.storage[this.numID] = value;
-  this.length++;
-  this.numID++;
+  this.storage[this.footer] = value;
+  this.footer++;
 }
 Queue.prototype.dequeue = function() {
-  let result = this.storage[this.header];
-  delete this.storage[this.header];
-  this.header++;
-  this.length > 0 ? this.length-- : this.length = 0;
-  return result;
+  if (this.footer - this.header > 0) {
+    let result = this.storage[this.header];
+    delete this.storage[this.header];
+    this.header++;
+    return result;
+  }
 }
